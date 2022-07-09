8e1ea5d818bda9dcd2e196d78726feba15221c4b
johnrearden <isnocturnal@gmail.com>
2022-07-09 09:40:11 +0000

    Mess with printing full terminal window

--- run.py
+++ run.py
@@ -1,130 +1,36 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+import time
 
 
-def get_sales_data():
-    """
-    Get sales figures from the user
-    """
-    while(True):
-        print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
-
-        data_str = input("Enter your data here: \n")
-
-        sales_data = data_str.split(",")
-
-        if validate_data(sales_data):
-            print("Data is valid")
-            break
-    return sales_data
-
-
-def validate_data(values):
-    """
-    Inside the try, converts all string values into integers.
-    Raised ValueError if strings cannot be converted into int, 
-    or if there aren't exactly 6 values
-    """
-    try:
-        [int(value) for value in values]
-        if len(values) != 6:
-            raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
-            )
-    except ValueError as err:
-        print(f"Invalid data: {err}, please try again")
-        return False
-    
-    return True
-
-
-def update_worksheet(data, sheet):
-    """
-    Update specified worksheet, add new row with the list data provided
-    """
-    print(f"Updating {sheet} worksheet")
-    worksheet_to_update = SHEET.worksheet(sheet)
-    worksheet_to_update.append_row(data)
-    print(f"{sheet.capitalize()} worksheet updated successfully\n")
+def create_board(width, height):
+    board = []
+    for ind in range(width * height):
+        board.append('.')
+        if ind % width == 0:
+            board.append('=\n')
+    return board
     
 
-def calculate_surplus_data(sales_row):
-    """
-    Compare sales with stock and calculate the surplus for each item type.
-    """
-    print("calculating surplus data \n")
-    stock = SHEET.worksheet('stock').get_all_values()
-    stock_row = stock[-1]
-    surplus_data = []
-    for stock, sales in zip(stock_row, sales_row):
-        surplus = int(stock) - sales
-        surplus_data.append(surplus)
-
-    return surplus_data
-
-
-def get_last_5_entries_sales():
-    """
-    Collects columns of data from sales worksheet, collecting 
-    the last 5 entries for each sandwich and returns the data 
-    as a list of lists
-    """
-    sales = SHEET.worksheet("sales")
-    
-    columns = []
-    for ind in range(1, 7):
-        column = sales.col_values(ind)
-        last_5_values = column[-5:]
-        columns.append(last_5_values)
-    return columns
-
-
-def calculate_stock_data(data):
-    """
-    Calculate the average stock for each item type, adding 10%
-    """
-    print("calculating stock data\n")
-    new_stock_data = []
-
-    for column in data:
-        int_column = [int(num) for num in column]
-        average = sum(int_column) / len(int_column)
-        stock_num = average * 1.1
-        new_stock_data.append(round(stock_num))
-    
-    return new_stock_data
+def stringify_board(board):
+    output = ''.join(board)
+    return output
 
 
 def main():
     """
     Run all program functions
     """
-    data = get_sales_data()
-    sales_data = [int(num) for num in data]
-    update_worksheet(sales_data, "sales")
-    new_surplus = calculate_surplus_data(sales_data)
-    update_worksheet(new_surplus, "surplus")
-    sales_columns = get_last_5_entries_sales()
-    stock_data = calculate_stock_data(sales_columns)
-    update_worksheet(stock_data, "stock")
-
-    print(stock_data)
+    board = create_board(60, 24)
+    output = stringify_board(board)
+    print(output)
+    for i in range(1, 10):
+        board[i] = '*'
+        board[i - 1] = '.'
+        output = stringify_board(board)
+        print(output)
+        time.sleep(0.1)
 
 
 print("Welcome to Love Sandwiches data automation")
